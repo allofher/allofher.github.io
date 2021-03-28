68d44e9cbe0e9289d4309c5b2c8d05b8e63c3a9a
allofher <46009557+allofher@users.noreply.github.com>
2021-03-28 17:51:55 -0600

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,15 +1,14 @@
-var request = new XMLHttpRequest()
-
-request.open('GET', 'https://api.chess.com/pub/player/allofher/stats', true)
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-
-  if (request.status >= 200 && request.status < 400) {
+fetch('https://api.chess.com/pub/player/allofher/stats')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    if (request.status >= 200 && request.status < 400) {
     data.forEach((stats) => {console.log(stats.rating)})
   } else {
-    console.log('error')
+    console.log('loop error')
   }
-}
-
-request.send()
+  })
+  .catch((err) => {
+    console.log('bigger error')
+  })
