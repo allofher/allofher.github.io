d688e28a4010812f8df13d0eb78c5dbe8ecc7708
allofher <46009557+allofher@users.noreply.github.com>
2021-03-28 17:45:23 -0600

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,14 +1,12 @@
 var request = new XMLHttpRequest()
 
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'https://api.chess.com/pub/player/allofher/stats', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
 
   if (request.status >= 200 && request.status < 400) {
-    data.forEach((movie) => {
-      console.log(movie.title)
-    })
+    data.forEach((stats) => {console.log(stats.rating)})
   } else {
     console.log('error')
   }
